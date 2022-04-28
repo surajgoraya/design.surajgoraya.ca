63a15ceb2da82340d82bc515feaf73abb6199581
surajgoraya <SurajGoraya@outlook.com>
2022-04-28 13:13:55 -0400

    feat: :tada: init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="application">
+      <h1>design.surajgoraya.ca</h1>
+      <h3>coming soon.</h3>
     </div>
   );
 }
